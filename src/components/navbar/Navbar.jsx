4386e3a299ab5c1ffb9fdb921fff5af32e2845c4
Carin Wood <carin.wood.85@gmail.com>
2022-07-29 11:35:53 +0200

    update responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import './navbar.css'
 import { NavLink } from 'react-router-dom'
 import Logo from '../assets/images/logoraw.png'
 import { BsSearch } from 'react-icons/bs'
+import { GrMenu } from 'react-icons/gr'
 import { useState } from 'react'
 import Search from '../Search'
 
@@ -14,6 +15,7 @@ const Navbar = () => {
   }
 
   return (
+    <>
     <nav className='navbar'>
         <ul className='nav-list'>
             <NavLink className='nav-item' to="/soppor">Soppor</NavLink>
@@ -31,8 +33,20 @@ const Navbar = () => {
             <div className='nav-item' onClick={() => toggleSearchFunc()}><BsSearch/></div>
             {toggleSearch && <Search toggleSearchFunc={toggleSearchFunc} />}
         </ul>
-           
+           <GrMenu className='hamburger-menu' />
     </nav>
+    <div className='responsive-menu'>
+      <ul className='responsive-nav'>
+            <NavLink className='responsive-item' to="/soppor">Soppor</NavLink>
+            <NavLink className='responsive-item' to="/sallader">Sallader</NavLink>
+            <NavLink className='responsive-item' to="/huvudratter">Huvudrätter</NavLink>
+            <NavLink className='responsive-item' to="/smoothies">Smoothies</NavLink>
+            <NavLink className='responsive-item' to="/efterratter">Efterrätter</NavLink>
+            <NavLink className='responsive-item' to="/tillbehor">Tillbehör</NavLink>
+            <NavLink className='responsive-item' to="/drinkar">Drinkar</NavLink>
+      </ul>
+    </div>
+    </>
   ) 
 }
 
